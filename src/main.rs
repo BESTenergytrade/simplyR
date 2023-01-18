f03079a9b288021b7f6975dedb33ce3834ae1924
m-yahya <yahya_gis@hotmail.com>
2023-01-18 09:48:40 +0100

    add methods to call algorithms

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,71 @@
-use clap::{Parser, ValueEnum};
 use simplyr_lib::{
-    custom_fair_matching, pay_as_bid_matching, GridFeeMatrix, GridFeeMatrixRaw, MarketInput,
+    custom_fair_matching, pay_as_bid_matching, GridFeeMatrix, MarketInput, MarketOutput, Order,
+    OrderType,
 };
-use std::fs::File;
-use std::io::BufReader;
-use std::path::PathBuf;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
-enum Algorithm {
-    PayAsBid,
-    CustomFair,
+fn pay_as_bid() -> MarketOutput {
+    // list of orders
+    let order_1 = Order {
+        id: 1,
+        order_type: OrderType::Ask,
+        time_slot: "2022-03-04T05:06:07+00:00".to_string(),
+        actor_id: "actor_1".to_string(),
+        cluster_index: Some(0),
+        energy_kwh: 2.0,
+        price_euro_per_kwh: 0.3,
+    };
+
+    let order_2 = Order {
+        id: 2,
+        order_type: OrderType::Bid,
+        time_slot: "2022-03-04T05:06:07+00:00".to_string(),
+        actor_id: "actor_2".to_string(),
+        cluster_index: Some(0),
+        energy_kwh: 1.5,
+        price_euro_per_kwh: 0.35,
+    };
+
+    // create a market input
+    let market_input = MarketInput {
+        orders: vec![order_1, order_2],
+    };
+
+    let pay_as_bid: MarketOutput = pay_as_bid_matching(&market_input);
+
+    pay_as_bid
 }
 
-/// Command line arguments
-#[derive(Parser, Clone, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Which matching algorithm to run
-    #[arg(short, long, value_name = "NAME")]
-    algo: Algorithm,
+fn custom_fair() -> MarketOutput {
+    // custom_fair_matching
+    let order_1 = Order {
+        id: 1,
+        order_type: OrderType::Ask,
+        time_slot: "2022-03-04T05:06:07+00:00".to_string(),
+        actor_id: "actor_1".to_string(),
+        cluster_index: Some(0),
+        energy_kwh: 2.0,
+        price_euro_per_kwh: 0.3,
+    };
 
-    /// Sets a the JSON file that includes the orders
-    #[arg(short, long, value_name = "FILE.json")]
-    orders: PathBuf,
+    let order_2 = Order {
+        id: 2,
+        order_type: OrderType::Bid,
+        time_slot: "2022-03-04T05:06:07+00:00".to_string(),
+        actor_id: "actor_2".to_string(),
+        cluster_index: Some(0),
+        energy_kwh: 1.5,
+        price_euro_per_kwh: 0.35,
+    };
 
-    /// Sets a the JSON file that includes the grid fee matrix (only used in custom fair matching)
-    #[arg(short, long, value_name = "FILE.json")]
-    grid_fee_matrix: Option<PathBuf>,
+    let grid_fee_matrix = GridFeeMatrix::from_json_str("[[0,1,1], [1,0,1], [1,1,0]]").unwrap();
 
-    /// Sets the energy unit (in kWh) that is used to divide Orders in our custom fair matching
-    #[arg(short, long, value_name = "NUM")]
-    energy_unit: Option<f64>,
+    let market_input = MarketInput {
+        orders: vec![order_1, order_2],
+    };
+
+    custom_fair_matching(&market_input, 1.0, &grid_fee_matrix)
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
-
-    match args.algo {
-        Algorithm::PayAsBid => {
-            let file = File::open(&args.orders)?;
-            let reader = BufReader::new(file);
-            let market_input: MarketInput = serde_json::from_reader(reader)?;
-            {
-                let market_output = pay_as_bid_matching(&market_input);
-                let mut stdout = std::io::stdout();
-                serde_json::to_writer_pretty(&mut stdout, &market_output)?;
-            }
-        }
-        Algorithm::CustomFair => {
-            let market_input: MarketInput = {
-                let file = File::open(&args.orders)?;
-                let reader = BufReader::new(file);
-                serde_json::from_reader(reader)?
-            };
-
-            let grid_fee_matrix: GridFeeMatrix = {
-                let file = File::open(args.grid_fee_matrix.unwrap())?;
-                let reader = BufReader::new(file);
-                let raw: GridFeeMatrixRaw = serde_json::from_reader(reader)?;
-                GridFeeMatrix::from_raw(&raw)?
-            };
-
-            {
-                let market_output = custom_fair_matching(
-                    &market_input,
-                    args.energy_unit.unwrap_or(1.0),
-                    &grid_fee_matrix,
-                );
-                let mut stdout = std::io::stdout();
-                serde_json::to_writer_pretty(&mut stdout, &market_output)?;
-            }
-        }
-    }
-
-    Ok(())
+fn main() {
+    println!("{:?}", custom_fair());
 }
